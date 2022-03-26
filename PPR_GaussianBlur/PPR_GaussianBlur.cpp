ca09dac9e73e596dfeb7ab1d31f9712c66fb8b8a
Aljoscha Alquati <aljoscha.alquati@gmail.com>
2022-03-26 15:05:00 +0100

    minor modifications

--- PPR_GaussianBlur/PPR_GaussianBlur.cpp
+++ PPR_GaussianBlur/PPR_GaussianBlur.cpp
@@ -10,15 +10,26 @@
 using namespace cv;
 using namespace std;
 
+const int NUM_THREADS = 16;
+
 //kernel generator from: https://gitlab.com/spook/Animator
 shared_ptr<float[]> generateKernel(int diameter)
 {
-    float sigma = 1;
+    int x, y, mean;
+    float sigma, sum;
+
+    sigma = 1;
     shared_ptr<float[]> kernel(new float[diameter * diameter]);
-    int mean = diameter / 2;
-    float sum = 0.0; // For accumulating the kernel values
-    for (int x = 0; x < diameter; ++x)
-        for (int y = 0; y < diameter; ++y) {
+    mean = diameter / 2;
+    sum = 0.0; // For accumulating the kernel values
+
+
+# pragma omp parallel \
+shared(sigma, mean, kernel) \
+private (x, y)
+# pragma omp for reduction(+:sum)
+    for (x = 0; x < diameter; ++x)
+        for (y = 0; y < diameter; ++y) {
             kernel[y * diameter + x] = (float)(exp(-0.5 * (pow((x - mean) / sigma, 2.0) + pow((y - mean) / sigma, 2.0))) / (2 * numbers::pi * sigma * sigma));
 
             // Accumulate the kernel values
@@ -26,8 +37,8 @@ shared_ptr<float[]> generateKernel(int diameter)
         }
 
     // Normalize the kernel
-    for (int x = 0; x < diameter; ++x)
-        for (int y = 0; y < diameter; ++y)
+    for (x = 0; x < diameter; ++x)
+        for (y = 0; y < diameter; ++y)
             kernel[y * diameter + x] /= sum;
 
     return kernel;
@@ -43,11 +54,11 @@ void gaussianBlur(unsigned char* inuputImage, unsigned char* outputImage, int wi
     int row, col, c, sum, x, y, color;
     float sumKernel, kernelValue;
 
-    //omp_set_num_threads(NUM_THREADS);
+    omp_set_num_threads(NUM_THREADS);
 # pragma omp parallel \
   shared ( diameter, radius, width, height) \
   private ( row, col, x, y, c, sum, color, kernelValue, sumKernel)
-# pragma omp for
+# pragma omp for schedule(dynamic)
     for (row = 0; row < height; row++)
     {
         for (col = 0; col < width; col++)
@@ -73,6 +84,7 @@ void gaussianBlur(unsigned char* inuputImage, unsigned char* outputImage, int wi
                 }
 
                 if (sumKernel > 0) {
+                    // no false sharing because every channel per pixel has a dedicated index in the array
                     outputImage[3 * row * width + 3 * col + c] = sum / sumKernel;
                 }
             }
@@ -87,23 +99,20 @@ int main()
 
     int radius = 9;
 
-    for (int i = 0; i < 20; i++)
+    for (int i = 0; i < 10; i++)
     {
-    //double start_time = omp_get_wtime();
-    chrono::steady_clock::time_point begin = chrono::steady_clock::now();
+    double start_time = omp_get_wtime();
 
-    //gaussianBlur(img.data, out.data, img.cols, img.rows, radius);
-    GaussianBlur(img, out, Size(radius, radius), 1.0);
+    gaussianBlur(img.data, out.data, img.cols, img.rows, radius);
+    //GaussianBlur(img, out, Size(radius, radius), 1.0);
 
-    chrono::steady_clock::time_point end = chrono::steady_clock::now();
-
-    long time = chrono::duration_cast<chrono::milliseconds>(end - begin).count();
-    //double time = omp_get_wtime() - start_time;
-    printf("Time: %ld milliseconds.\n", time);
+    double end = omp_get_wtime();
+    double time = end - start_time;
+    printf("Time: %lf milliseconds.\n", time * 1000);
     }
 
-    //imshow("img", img);
-    //imshow("out", out);
+    //imshow("img", img);/*
+    //imshow("out", out);*/
     waitKey(0);
 
     return 0;
